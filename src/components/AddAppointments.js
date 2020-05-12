fbee8daa44d4e2b7864ee621d019d4aec371e843
Navid <nkhalili@live.com>
2020-05-12 12:19:56 -0400

    AddAppointments state change

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react'
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
 class AddAppointments extends Component {
+  constructor() {
+    super()
+    this.state = {
+      petName: '',
+      ownerName: '',
+      aptDate: '',
+      aptTime: '',
+      aptNotes: ''
+    }
+  }
+
+  handleChange = (e) => {
+    const target = e.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
       <div className={
@@ -29,6 +50,8 @@ class AddAppointments extends Component {
                 className="form-control"
                 name="petName"
                 placeholder="Pet's Name"
+                value={this.state.petName}
+                onChange={this.handleChange}
               />
             </div>
           </div>
@@ -46,6 +69,8 @@ class AddAppointments extends Component {
                 className="form-control"
                 name="ownerName"
                 placeholder="Owner's Name"
+                value={this.state.ownerName}
+                onChange={this.handleChange}
               />
             </div>
           </div>
@@ -63,6 +88,8 @@ class AddAppointments extends Component {
                 className="form-control"
                 name="aptDate"
                 id="aptDate"
+                value={this.state.aptDate}
+                onChange={this.handleChange}
               />
             </div>
             <label
@@ -77,6 +104,8 @@ class AddAppointments extends Component {
                 className="form-control"
                 name="aptTime"
                 id="aptTime"
+                value={this.state.aptTime}
+                onChange={this.handleChange}
               />
             </div>
           </div>
@@ -93,6 +122,8 @@ class AddAppointments extends Component {
                 name="aptNotes"
                 id="aptNotes"
                 placeholder="Appointment Notes"
+                value={this.state.aptNotes}
+                onChange={this.handleChange}
               />
             </div>
           </div>
