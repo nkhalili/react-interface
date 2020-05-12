efdbca13e83336d0df2a60c09a54488fbb774716
Navid <nkhalili@live.com>
2020-05-11 21:18:36 -0400

    Rendering objects

--- src/components/App.js
+++ src/components/App.js
@@ -28,21 +28,30 @@ class App extends React.Component {
   }
 
   render(){
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+
+    const listItems = this.state.myAppointments.map(item => (
+      <div>
+        <div>{item.petName}</div>
+        <div>{item.ownerName}</div>
+      </div>
+    ))
+
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                { listItems }
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  )}
+      </main>
+    )}
 }
 
 export default App;
