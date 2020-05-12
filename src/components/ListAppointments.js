2580bc0fdd9cd596cf1e7d6c1e47f44b534b4cf3
Navid <nkhalili@live.com>
2020-05-11 21:56:36 -0400

    Added index to appointment list

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -5,8 +5,8 @@ class ListAppointments extends Component {
 
     return (
       <div className="appointment-list item-list mb-3">
-        {this.props.appointments.map(item => (
-          <div className="pet-item col media py-3">
+        {this.props.appointments.map((item, i) => (
+          <div className="pet-item col media py-3" key={i}>
             <div className="mr-3">
               <button className="pet-delete btn btn-sm btn-danger">X</button>
             </div>
